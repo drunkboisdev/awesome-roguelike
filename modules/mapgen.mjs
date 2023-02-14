bc971424a041935bfc7c241590bc0abb8812a3af
QuantumFlare <mangopies69@gmail.com>
2023-02-14 11:11:11 -0500

    move displayMap to module

--- modules/mapgen.mjs
+++ modules/mapgen.mjs
@@ -1,5 +1,6 @@
+let map = []
+
 function generateMap(size) {
-    let map = []
     size = size * 2 + 1
 
     for (let i = 0; i < size; i++) {
@@ -11,4 +12,28 @@ function generateMap(size) {
     return map
 }
 
-export {generateMap}
+function displayMap() {
+    for (let i = 0; i < 11; i++) {
+        for (let j = 0; j < 11; j++) {
+            if (map[i][j] === 0) {
+                process.stdout.write("██")
+            } else if (map[i][j] === 3) {
+                process.stdout.write("Tr")
+            } else if (map[i][j] === 4) {
+                process.stdout.write("Sh")
+            } else if (map[i][j] === 5) {
+                process.stdout.write("En")
+            } else if (map[i][j] === 6) {
+                process.stdout.write("St")
+            } else if (map[i][j] === 7) {
+                process.stdout.write("Fn")
+            }
+            else {
+                process.stdout.write("  ")
+            }
+        }
+        process.stdout.write("\n")
+    }
+}
+
+export {generateMap, displayMap}
--- thingy.js
+++ thingy.js
@@ -1,7 +1,7 @@
 "use strict"
 
 import * as Map from "./modules/mapgen.mjs"
-import PromptSync from "prompt-sync"
+import PromptSync from "prompt-sync" // i have no idea how this works and i'm too scared to ask
 const prompt = PromptSync()
 // TIME SPENT: 6 HOURS
 
@@ -13,30 +13,6 @@ const prompt = PromptSync()
 
     }
 }*/
-
-function displayMap(map) {
-    for (let i = 0; i < 11; i++) {
-        for (let j = 0; j < 11; j++) {
-            if (map[i][j] === 0) {
-                process.stdout.write("██")
-            } else if (map[i][j] === 3) {
-                process.stdout.write("Tr")
-            } else if (map[i][j] === 4) {
-                process.stdout.write("Sh")
-            } else if (map[i][j] === 5) {
-                process.stdout.write("En")
-            } else if (map[i][j] === 6) {
-                process.stdout.write("St")
-            } else if (map[i][j] === 7) {
-                process.stdout.write("Fn")
-            }
-            else {
-                process.stdout.write("  ")
-            }
-        }
-        process.stdout.write("\n")
-    }
-}
 
 function battle(floorNum) {
     let enemies = []
@@ -66,7 +42,7 @@ function battle(floorNum) {
 function game() {
     console.log("hi")
     const map = Map.generateMap(5)
-    displayMap(map)
+    Map.displayMap()
 }
 
 console.log("\nit's time for our trials to begin.")
