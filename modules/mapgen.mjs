b7751ed540d43ffe9a532c349b38a2d9d3abdf9e
QuantumFlare <beearchetypesandco@gmail.com>
2023-02-21 15:50:01 -0500

    generate multiple paths

--- modules/mapgen.mjs
+++ modules/mapgen.mjs
@@ -31,60 +31,72 @@ function generateMap(size) {
     let visiting = [1,1]
     let walk = [[1,1]]
 
-    // loop-erased random walk
-    while (!areCoordsEqual(visiting, end)) {
-        const rand = Math.random()
+    while (unvisitedCells[0]) {
+        // loop-erased random walk
+        while (!areCoordsEqual(visiting, end)) {
+            const rand = Math.random()
 
-        // randomly walking
-        if (rand < 0.25) {
-            visiting = [walk[walk.length-1][0] + 2, walk[walk.length-1][1]]
-            if (visiting[0] < map[0].length) {
-                walk.push([visiting[0]-1, visiting[1]])
-                walk.push(visiting)
+            // randomly walking
+            if (rand < 0.25) {
+                visiting = [walk[walk.length-1][0] + 2, walk[walk.length-1][1]]
+                if (visiting[0] < map[0].length) {
+                    walk.push([visiting[0]-1, visiting[1]])
+                    walk.push(visiting)
+                }
+            } else if (rand < 0.5) {
+                visiting = [walk[walk.length-1][0], walk[walk.length-1][1] + 2]
+                if (visiting[1] < map[1].length) {
+                    walk.push([visiting[0], visiting[1]-1])
+                    walk.push(visiting)
+                }
+            } else if (rand < 0.75) {
+                visiting = [walk[walk.length-1][0] - 2, walk[walk.length-1][1]]
+                if (visiting[0] > 0) {
+                    walk.push([visiting[0]+1, visiting[1]])
+                    walk.push(visiting)
+                }
+            } else {
+                visiting = [walk[walk.length-1][0], walk[walk.length-1][1] - 2]
+                if (visiting[1] > 0) {
+                    walk.push([visiting[0], visiting[1]+1])
+                    walk.push(visiting)
+                }
             }
-        } else if (rand < 0.5) {
-            visiting = [walk[walk.length-1][0], walk[walk.length-1][1] + 2]
-            if (visiting[1] < map[1].length) {
-                walk.push([visiting[0], visiting[1]-1])
-                walk.push(visiting)
-            }
-        } else if (rand < 0.75) {
-            visiting = [walk[walk.length-1][0] - 2, walk[walk.length-1][1]]
-            if (visiting[0] > 0) {
-                walk.push([visiting[0]+1, visiting[1]])
-                walk.push(visiting)
-            }
-        } else {
-            visiting = [walk[walk.length-1][0], walk[walk.length-1][1] - 2]
-            if (visiting[1] > 0) {
-                walk.push([visiting[0], visiting[1]+1])
-                walk.push(visiting)
-            }
-        }
 
-        // destroy loops
-        for (let i = 0; i < walk.length - 1; i += 2) {
-            if (areCoordsEqual(walk[i], walk[walk.length-1])) {
-                for (let j = walk.length - 1; j > i; j--) {
-                    walk.pop()
+            // destroy loops
+            for (let i = 0; i < walk.length - 1; i += 2) {
+                if (areCoordsEqual(walk[i], walk[walk.length-1])) {
+                    for (let j = walk.length - 1; j > i; j--) {
+                        walk.pop()
+                    }
                 }
             }
         }
-    }
 
-    // make changes to map (and unvisited cells)
-    for (let i = 0; i < walk.length; i++) {
-        if (walk[i][0] % 2 === 0 || walk[i][1] % 2 === 0) {
-            map[walk[i][0]][walk[i][1]] = 1
-        } else {
-            map[walk[i][0]][walk[i][1]] = 2
-        }
-        // if walk and unvisitedcells share elements, remove them from unvisitedCells
-        for (let j = 0; j < unvisitedCells.length; j++) {
-            if (areCoordsEqual(walk[i], unvisitedCells[j])) {
-                unvisitedCells.splice(j, 1)
+        // make changes to map (and unvisited cells)
+        for (let i = 0; i < walk.length; i++) {
+            if (walk[i][0] % 2 === 0 || walk[i][1] % 2 === 0) {
+                map[walk[i][0]][walk[i][1]] = 1
+            } else {
+                map[walk[i][0]][walk[i][1]] = 2
+            }
+            // if walk and unvisitedcells share elements, remove them from unvisitedCells
+            for (let j = 0; j < unvisitedCells.length; j++) {
+                if (areCoordsEqual(walk[i], unvisitedCells[j])) {
+                    unvisitedCells.splice(j, 1)
+                }
             }
         }
+
+        // update variables for next pass
+        start = walk[Math.floor(Math.random() * walk.length / 2) * 2]
+        end = unvisitedCells[Math.floor(Math.random() * unvisitedCells.length)]
+        console.log(walk)
+        console.log(unvisitedCells)
+        visiting = start
+        walk = [start]
+        console.log(`s: ${start}`)
+        console.log(`e: ${end}`)
     }
 
     return map
