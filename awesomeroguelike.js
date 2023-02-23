efa615a506a7115c7977761fb9eebc4327240cdb
QuantumFlare <beearchetypesandco@gmail.com>
2023-02-22 21:12:41 -0500

    start work on commands
    add quit and test commands

--- awesomeroguelike.js
+++ awesomeroguelike.js
@@ -5,9 +5,24 @@ import PromptSync from "prompt-sync" // i have no idea how this works and i'm to
 const prompt = PromptSync() // "esm default" "commonjs" "module.export()" none of these words are in the bible
 // TIME SPENT: 11 HOURS
 
+let stats = {
+    maxHP: 20
+}
+
 function game() {
-    const map = Map.generateMap(5)
+    const map = Map.generateMap(16)
     Map.displayMap()
+    let input = prompt("what do you want to do? ")
+    while (input !== "quit") {
+        switch (input) {
+            case "stats":
+                console.log("heyyyy")
+                break
+            default:
+                console.log("that's not a valid command!")
+        }
+        input = prompt("what do you want to do? ")
+    }
 }
 
 console.log("it's time for our trials to begin.")
